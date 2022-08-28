2c619f275bb03f29c4d86c0adefdc9995cb41992
Daxesh Solanki <87466510+SolankiDaxesh@users.noreply.github.com>
2022-08-28 18:23:29 +0530

    commit

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,14 +1,15 @@
 
 import { Component, OnInit } from '@angular/core';
 import { HousingService } from 'src/app/services/housing.service';
-
+import { HttpClient } from '@angular/common/http';
+import { IProperty } from '../IProperty.interface';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-  properties: any;;
+  properties: Array<IProperty> | undefined;
   constructor(private housingService: HousingService) { }
 
   ngOnInit(): void {
@@ -16,7 +17,11 @@ export class PropertyListComponent implements OnInit {
       data => {
         this.properties = data;
         console.log(data)
+      },
+      error=>{
+        console.log('httperror: ');
+        console.log(error)
       }
-    )
+    );
   }
 }
